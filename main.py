b33201afd1a64fc31ae8dbc46db74432acae356c
AndreaAbed <126136333+AndreaAbed@users.noreply.github.com>
2024-04-12 13:31:15 -0700

    added imports

--- main.py
+++ main.py
@@ -31,3 +31,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+import tensorflow as tensor
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tensor.__version__)

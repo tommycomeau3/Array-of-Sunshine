21cce4d32e51b97b839add7d84f4db8038697fb7
Pranav <108417418+pnambiar24770@users.noreply.github.com>
2024-04-15 20:51:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -38,3 +38,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 print(tensor.__version__)
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
